7730068e09371e662c97114df96fd47a9f995834
Ollie <oliver.beckwith1@hotmail.com>
2020-10-30 10:29:49 +0000

    Created default admin account

--- webappdemo.sql
+++ webappdemo.sql
@@ -21,4 +21,7 @@ CREATE TABLE admins (
 	id INTEGER PRIMARY KEY AUTOINCREMENT,
 	`password` VARCHAR(255) NOT NULL,
 	salt VARCHAR(255) NOT NULL
-)
+);
+
+-- Default admin account so that there is always at least one.
+INSERT INTO admins (id,`password`, salt) VALUES (1,'3031363367a96566bcab97a978a0b9a3f3dfbf688a730285129c5387349449ec6784714a3ae9c989ec2e01eff575d7ce96cc335f1bee3158cccc95182c63c2d3','88d0682061d0539e');
